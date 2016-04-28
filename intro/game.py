6882f2f55013f01acc11773c38bedf62be6018d7
Huib Keemink <huib.keemink@creativedutchmen.com>
2016-04-28 11:04:56 +0200

    done\!

--- intro/game.py
+++ intro/game.py
@@ -3,8 +3,108 @@ class Game:
     Simple bowling game class to demonstrate TDD in Python
     """
 
-    def roll(self, pins):
-        pass
+    def __init__(self):
+        self._frames = []
+        self._rules = [
+            RegularRule(),
+            SpareRule(),
+            StrikeRule(),
+        ]
+
+    def add_frame(self, frame):
+        frames = list(self._frames)
+        frames.append(frame)
+        for rule in self._rules:
+            rule.check_valid(frames)
+        self._frames = frames
 
     def score(self):
-        return 0
+        score = 0
+        for rule in self._rules:
+            score += rule.calculate_score(self._frames)
+        return score
+
+
+class Scorer(object):
+    """
+    Abstract Scorer class, to be implemented by all Rules
+    """
+    def calculate_score(self, frames):
+        raise NotImplementedError()
+
+
+class Validator(object):
+    """
+    Abstract Validator class, to be implemented by all Rules
+    """
+    def check_valid(self, frames):
+        raise NotImplementedError()
+
+
+class Rule(Scorer, Validator):
+    """
+    Abstract Rule class, to be implemented by all Rules
+    """
+    pass
+
+
+class FillBallValidator(Validator):
+    """
+    Abstract class implementing the fill ball rule
+    """
+    def check_valid(self, frames):
+        for index, frame in enumerate(frames):
+            if len(frame) > 2 and index != 9:
+                raise ValueError('Fill ball only allowed in the last frame')
+            if len(frame) > 2 and (frame[0] + frame[1]) < 10:
+                raise ValueError('There is no fill ball with an open frame')
+
+
+class RegularRule(Rule):
+    def check_valid(self, frames):
+        if not(0 < len(frames) <= 10):
+            raise ValueError('Game lasts for a maximum of 10 frames')
+        if max([max(frame) for frame in frames]) > 10:
+            raise ValueError('There are only ten pins')
+        if min([min(frame) for frame in frames]) < 0:
+            raise ValueError('You can\'t knock over negative pins')
+        return True
+
+    def calculate_score(self, frames):
+        score = 0
+        for frame in frames:
+            score += sum(frame)
+        return score
+
+
+class SpareRule(FillBallValidator, Rule):
+    def calculate_score(self, frames):
+        score = 0
+        for index, frame in enumerate(frames):
+            if sum(frame) == 10:
+                try:
+                    score += frames[index + 1][0]
+                except IndexError:
+                    pass
+        return score
+
+
+class StrikeRule(FillBallValidator, Rule):
+    def check_valid(self, frames):
+        # Run the valid check on the parent
+        super(StrikeRule, self).check_valid(frames)
+        for index, frame in enumerate(frames):
+            if ((frame[0] + frame[1]) > 10) and index != 9:
+                raise ValueError('If you throw a strike your frame is over')
+
+    def calculate_score(self, frames):
+        score = 0
+        for index, frame in enumerate(frames):
+            if frame[0] == 10:
+                try:
+                    score += frames[index + 1][1]
+                    if frames[index + 1][0] == 10:
+                        score += frames[index + 2][0]
+                except IndexError:
+                    pass
+        return score
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,5 +1,5 @@
-from intro.game import Game
-from nose.tools import assert_equals
+from intro.game import Game, Rule
+from nose.tools import assert_equals, raises
 
 
 class testGame():
@@ -12,6 +12,114 @@ class testGame():
     def setup(self):
         self._game = Game()
 
-    def test_roll_zero_get_zero(self):
-        self._game.roll(0)
+    def test_start_zero(self):
         assert_equals(self._game.score(), 0)
+
+    def test_gutter_game(self):
+        self._throw_multiple(n=10, frame=(0, 0))
+        assert_equals(self._game.score(), 0)
+
+    def test_ones_game(self):
+        self._throw_multiple(n=10, frame=(1, 1))
+        assert_equals(self._game.score(), 20)
+
+    def test_spare(self):
+        self._game.add_frame((5, 5))
+        self._game.add_frame((7, 1))
+        assert_equals(self._game.score(), 17+8)
+
+    def test_strike(self):
+        self._game.add_frame((10, 0))
+        self._game.add_frame((1, 1))
+        assert_equals(self._game.score(), 14)
+
+    def test_last_throw_spare(self):
+        self._throw_multiple(n=9, frame=(0, 0))
+        self._game.add_frame((5, 5, 5))
+        assert_equals(self._game.score(), 15)
+
+    def test_last_throw_spare_gutter(self):
+        self._throw_multiple(n=9, frame=(0, 0))
+        self._game.add_frame((5, 5, 0))
+        assert_equals(self._game.score(), 10)
+
+    def test_last_throw_strike(self):
+        self._throw_multiple(n=9, frame=(0, 0))
+        self._game.add_frame((10, 5, 5))
+        assert_equals(self._game.score(), 20)
+
+    def test_perfect_game(self):
+        self._throw_multiple(n=9, frame=(10, 0))
+        self._game.add_frame((10, 10, 10))
+        assert_equals(self._game.score(), 300)
+
+    """
+    Test negative paths
+    """
+    @raises(ValueError)
+    def test_long_game(self):
+        self._throw_multiple(n=11, frame=(1, 1))
+
+    @raises(ValueError)
+    def test_many_pins(self):
+        self._game.add_frame((11, 0))
+
+    @raises(ValueError)
+    def test_three_throws(self):
+        self._game.add_frame((10, 10, 10))
+
+    @raises(ValueError)
+    def test_three_throws_without_spare_or_strike(self):
+        self._throw_multiple(n=9, frame=(1, 1))
+        self._game.add_frame((1, 1, 1))
+
+    @raises(ValueError)
+    def test_negative_score(self):
+        self._game.add_frame((-1, 1))
+
+    @raises(ValueError)
+    def test_two_lanes(self):
+        self._game.add_frame((10, 5))
+
+    def invalid_input_is_not_saved(self):
+        try:
+            self._game.add_frame((10, 5))
+        except ValueError:
+            assert_equals(self._game.score(), 0)
+
+    """
+    Testing the fun part: adding new rules on the fly
+    """
+    def test_add_rule_before(self):
+        self._throw_multiple(n=3, frame=(10, 0))
+
+    @raises(Exception)
+    def test_add_rule_after(self):
+        # Note I am writing to a variable I should not be writing to.
+        # It would be cleaner to extend the Game class and add it to the
+        # __init__ function. However, for the test this is fine.
+        self._game._rules.append(ThreeStrikesOut())
+        self._throw_multiple(n=3, frame=(10, 0))
+
+    """
+    Helpers
+    """
+    def _throw_multiple(self, n, frame):
+        for i in range(0, n):
+            self._game.add_frame(frame)
+
+
+class ThreeStrikesOut(Rule):
+    """
+    Testing the ability to add new rules
+    """
+    def is_valid(self, frames):
+        strikes = 0
+        for frame in frames:
+            if frame[0] == 10:
+                strikes += 1
+        if strikes >= 3:
+            raise Exception('You\'re out!')
+
+    def calculate_score(self, frames):
+        pass
